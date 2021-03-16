bbeebd077f0a26a3efa2e6f81391f6bf9d75bdc4
katherine-p <c14180221@john.petra.ac.id>
2021-03-16 23:38:44 +0800

    photogallery2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,27 @@ export class Tab2Page {
 
   constructor() {}
 
+  num;
+  randnum = Math.floor(Math.random() * 20);
+  cek = 0;
+
+  ceknum()
+  {
+    console.log(this.randnum);
+    if(this.num == this.randnum)
+    {
+      console.log("samaa");
+      this.cek = 1;
+
+    }
+    else if(this.num != this.randnum)
+    {
+      console.log("beda");
+      this.cek = 0;
+      alert("Oops try again:)");
+
+    }    
+
+  }
+
 }
